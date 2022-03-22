80bede01022fb2470eb3289ad726a2677350a076
chelorope <cmarcelorp@gmail.com>
2022-03-22 01:23:04 -0300

    part2 - reactivity functions

--- part-2/reactivity.js
+++ part-2/reactivity.js
@@ -1,5 +1,67 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const title = document.querySelector("h1");
+  const titleElem = document.querySelector("#title");
+  const quantityInput = document.querySelector("#input-quant");
+  const priceInput = document.querySelector("#input-price");
+  const totalElem = document.querySelector("#total");
 
-  console.log(title);
+  let activeEffect = null;
+  const subscriptions = new WeakMap();
+
+  function getSubscribersForProperty(target, key) {
+    if (!subscriptions[target]) {
+      subscriptions[target] = new Map();
+    }
+
+    if (!subscriptions[target][key]) {
+      subscriptions[target][key] = new Set();
+    }
+    return subscriptions[target][key];
+  }
+
+  function track(target, key) {
+    if (activeEffect) {
+      const effects = getSubscribersForProperty(target, key);
+      effects.add(activeEffect);
+    }
+  }
+
+  function trigger(target, key) {
+    const effects = getSubscribersForProperty(target, key);
+    effects.forEach((effect) => effect());
+  }
+
+  function whatchEffect(update) {
+    const effect = () => {
+      activeEffect = effect;
+      update();
+      activeEffect = null;
+    };
+    effect();
+  }
+
+  const reactive = (object) => {
+    const handler = {
+      get(target, property, receiver) {
+        track(target, property);
+        console.log(target, property);
+        return Reflect.get(target, property, receiver);
+      },
+      set(target, property, value, receiver) {
+        trigger(target, property);
+        console.log(`${property}: ${value}`);
+        return Reflect.set(target, property, value, receiver);
+      },
+    };
+    return new Proxy(object, handler);
+  };
+
+  const state = reactive({ quantity: 0, price: 0 });
+
+  quantityInput.value = state.price;
+
+  priceInput.oninput = (event) => {
+    state.price = event.target.value;
+  };
+
+  console.log(person);
 });
