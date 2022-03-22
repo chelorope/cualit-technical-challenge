94eb867f2fc4a2a78e2b9871f68055157620ab6a
chelorope <cmarcelorp@gmail.com>
2022-03-22 02:50:07 -0300

    part2 - encapsulate functions in closure / frontend syling

--- part-2/app.js
+++ part-2/app.js
@@ -1,22 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const titleElem = document.querySelector("#title");
   const quantityInput = document.querySelector("#input-quant");
   const priceInput = document.querySelector("#input-price");
   const totalElem = document.querySelector("#total");
 
   const state = reactive({ quantity: 0, price: 0 });
 
+  watchEffect(() => {
+    totalElem.innerHTML = `Quantity x Price = ${state.quantity * state.price}`;
+  });
+
   priceInput.value = state.price;
   quantityInput.value = state.quantity;
 
-  whatchEffect(() => {
-    console.log("Quantity", state.quantity);
-    console.log("Price:", state.price);
-    totalElem.innerHTML = `${state.quantity * state.price}`;
-  });
-
   priceInput.oninput = (event) => {
-    console.log(event.target.value);
     state.price = Number(event.target.value);
   };
 
--- part-2/reactivity.js
+++ part-2/reactivity.js
@@ -1,48 +1,52 @@
-let activeEffect = null;
-const subscriptions = new WeakMap();
+const { reactive, watchEffect } = (() => {
+  let activeEffect = null;
+  const subscriptions = new WeakMap();
 
-function getSubscribersForProperty(target, key) {
-  if (!subscriptions.get(target)) {
-    subscriptions.set(target, new Map());
+  function getSubscribersForProperty(target, key) {
+    if (!subscriptions.get(target)) {
+      subscriptions.set(target, new Map());
+    }
+
+    if (!subscriptions.get(target).get(key)) {
+      subscriptions.get(target).set(key, new Set());
+    }
+    return subscriptions.get(target).get(key);
   }
 
-  if (!subscriptions.get(target).get(key)) {
-    subscriptions.get(target).set(key, new Set());
+  function track(target, key) {
+    if (activeEffect) {
+      const effects = getSubscribersForProperty(target, key);
+      effects.add(activeEffect);
+    }
   }
-  return subscriptions.get(target).get(key);
-}
 
-function track(target, key) {
-  if (activeEffect) {
+  function trigger(target, key) {
     const effects = getSubscribersForProperty(target, key);
-    effects.add(activeEffect);
+    effects.forEach((effect) => effect());
   }
-}
 
-function trigger(target, key) {
-  const effects = getSubscribersForProperty(target, key);
-  effects.forEach((effect) => effect());
-}
-
-const reactive = (object) => {
-  const handler = {
-    get(target, key, receiver) {
-      track(target, key);
-      return Reflect.get(target, key, receiver);
+  return {
+    reactive: (object) => {
+      const handler = {
+        get(target, key, receiver) {
+          track(target, key);
+          return Reflect.get(target, key, receiver);
+        },
+        set(target, key, value, receiver) {
+          Reflect.set(target, key, value, receiver);
+          trigger(target, key);
+        },
+      };
+      return new Proxy(object, handler);
     },
-    set(target, key, value, receiver) {
-      Reflect.set(target, key, value, receiver);
-      trigger(target, key);
+
+    watchEffect: (update) => {
+      const effect = () => {
+        activeEffect = effect;
+        update();
+        activeEffect = null;
+      };
+      effect();
     },
   };
-  return new Proxy(object, handler);
-};
-
-function whatchEffect(update) {
-  const effect = () => {
-    activeEffect = effect;
-    update();
-    activeEffect = null;
-  };
-  effect();
-}
+})();
